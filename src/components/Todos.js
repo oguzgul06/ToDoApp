971bc3309b251d60bb25ed49187a16d1557ab403
Oğuz GÜL <ogzgl06@gmail.com>
2022-01-09 16:03:38 +0300

    Create redux structure and AddReducer

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Todos() {
-  return <div className="addTodos">
-      <input type="text" className="todo-input"/>
-  </div>;
+  const [todo, setTodo] = useState("");
+
+  const handleChange = (event) => {
+    setTodo(event.target.value);
+  };
+
+  return (
+    <div className="addTodos">
+      <input
+        onChange={(e) => handleChange(e)}
+        type="text"
+        className="todo-input"
+      />
+      <button className="add-btn">Add</button>
+    </div>
+  );
 }
 
 export default Todos;
