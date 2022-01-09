4dfa77f90a30b59e3059cd60963efdce44b2ca91
Oğuz GÜL <ogzgl06@gmail.com>
2022-01-09 16:26:12 +0300

    Mapping todolist

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,11 +1,27 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import { addTodos } from "../redux/reducer";
 
-function Todos() {
+const mapStateToProps = (state) => {
+  return {
+    todos: state,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    addTodo: (obj) => dispatch(addTodos(obj)),
+  };
+};
+
+function Todos(props) {
   const [todo, setTodo] = useState("");
 
   const handleChange = (event) => {
     setTodo(event.target.value);
   };
+
+  console.log("props from store", props);
 
   return (
     <div className="addTodos">
@@ -14,9 +30,28 @@ function Todos() {
         type="text"
         className="todo-input"
       />
-      <button className="add-btn">Add</button>
+      <button
+        onClick={() =>
+          props.addTodo({
+            id: Math.floor(Math.random() * 1000),
+            item: todo,
+            completed: false,
+          })
+        }
+        className="add-btn"
+      >
+        Add
+      </button>
+      <br />
+
+      <ul>
+        {props.todos.map((item) => {
+          return <li key={item.id}>{item.item}</li>;
+        })}
+      </ul>
     </div>
   );
 }
 
-export default Todos;
+//We can use connect method to connect this component with redux store
+export default connect(mapStateToProps, mapDispatchToProps)(Todos);
